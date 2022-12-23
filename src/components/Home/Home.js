23ec14e4b3a17a37416167533740508227c6af88
Abu Raihan <raihanahmed01973@gmail.com>
2022-12-23 20:29:31 +0600

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import navLogo from "../../gymLogo.png";
+import "./home.css";
+const Home = () => {
+  return (
+    <div>
+      <div className="title-area">
+        <img src={navLogo} className="navLogo" alt="" /> <h2>Gymmy boody</h2>
+      </div>
+      <h3 style={{marginLeft:'30px'}}>Select today exercise</h3>
+
+{/* main content start from here  */}
+ 
+
+    </div>
+  );
+};
+
+export default Home;
